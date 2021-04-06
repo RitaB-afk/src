580fc0c79e461787b811928cb4621a2eb68c6838
RitaB-afk <77437092+RitaB-afk@users.noreply.github.com>
2021-04-06 21:05:55 +0200

    ny

--- JFrameDisableButton.java
+++ JFrameDisableButton.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -10,12 +11,18 @@ public class JFrameDisableButton  extends JFrame implements ActionListener {
     public JFrameDisableButton () {
         super ("Disabled Button");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
+        setSize(WIDTH,HEIGHT);
         add(press);
         press.addActionListener(this);
+    }
+    public static void main(String[] args) {
+        JFrameDisableButton ex= new JFrameDisableButton();
+        ex.setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        
+        press.setEnabled(false);
     }
 }
